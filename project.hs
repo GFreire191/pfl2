72fdae31d239e779d7d9183cf607decc79f6d26e
Freirex101 <guilodinho@gmail.com>
2023-12-26 21:34:43 +0000

    Cena do true / false

--- project.hs
+++ project.hs
@@ -1,10 +1,10 @@
-import Data.List (intercalate, sort)
+import Data.List (intercalate, sort, deleteBy)
 import Data.Maybe (fromMaybe)
 
 -- PFL 2023/24 - Haskell practical assignment quickstart
 
 -- Do not modify our definition of Inst and Code
-data Inst
+data Inst 
   = Push Integer
   | Add
   | Mult
@@ -24,80 +24,77 @@ data Inst
 
 type Code = [Inst]
 
-type Stack = [Integer]
+type Stack = [(Integer, Bool)]
 
-type State = [(String, Integer)]
+type State = [(String, (Integer, Bool))]
 
--- createEmptyStack :: Stack
 createEmptyStack :: Stack
 createEmptyStack = []
 
--- stack2Str :: Stack -> String
 stack2Str :: Stack -> String
 stack2Str stack = intercalate "," (map valToStr stack)
   where
-    valToStr 0 = "False"
-    valToStr 1 = "True"
-    valToStr n = show n
+    valToStr (1, True) = "True"
+    valToStr (0, _) = "False"
+    valToStr (n, _) = show n
 
--- createEmptyState :: State
 createEmptyState :: State
 createEmptyState = []
 
--- state2Str :: State -> String
 state2Str :: State -> String
-state2Str state = intercalate "," [var ++ "=" ++ valToStr var val | (var, val) <- sort state]
+state2Str state = intercalate "," [var ++ "=" ++ valToStr val | (var, val) <- sort state]
   where
-    valToStr var 1 = if var == "i" then "1" else "True"
-    valToStr _ 0 = "False"
-    valToStr _ n = show n
+    valToStr (1, True) = "True"
+    valToStr (0, _) = "False"
+    valToStr (n, _) = show n
 
--- run :: (Code, Stack, State) -> (Code, Stack, State)
 run :: (Code, Stack, State) -> (Code, Stack, State)
 run ([], stack, state) = ([], stack, state)
 run (inst : rest, stack, state) =
   case inst of
-    Push n -> run (rest, n : stack, state)
+    Push n -> run (rest, (n, False) : stack, state)
     Add -> case stack of
-      x : y : stack' -> run (rest, (if x /= 0 && y /= 0 then 1 else 0) : stack', state)
+      (x, _) : (y, _) : stack -> run (rest, (x + y, False) : stack, state)
       _ -> error "Run-time error"
     Mult -> case stack of
-      x : y : stack' -> run (rest, (x * y) : stack', state)
+      (x, _) : (y, _) : stack -> run (rest, (x * y, False) : stack, state)
       _ -> error "Run-time error"
     Sub -> case stack of
-      x : y : stack' -> let res = if x == 1 && y == 1 then 1 else x - y in run (rest, res : stack', state)
+      (x, _) : (y, _) : stack -> run (rest, (x - y, False) : stack, state)
       _ -> error "Run-time error"
-    Tru -> run (rest, 1 : stack, state)
-    Fals -> run (rest, 0 : stack, state)
+    Tru -> run (rest, (1, True) : stack, state)
+    Fals -> run (rest, (0, True) : stack, state)
     Equ -> case stack of
-      x : y : stack' -> run (rest, (if x == y then 1 else 0) : stack', state)
+      (x, _) : (y, _) : stack -> run (rest, (if x == y then (1, True) else (0, True)) : stack, state)
       _ -> error "Run-time error"
     Le -> case stack of
-      x : y : stack' -> run (rest, (if x <= y then 1 else 0) : stack', state)
+      (x, _) : (y, _) : stack -> run (rest, (if x <= y then (1, True) else (0, True)) : stack, state)
       _ -> error "Run-time error"
     And -> case stack of
-      x : y : stack' -> run (rest, (if x /= 0 && y /= 0 then 1 else 0) : stack', state)
-      _ -> error "Run-time error"
+      (x, _) : (y, _) : stack -> 
+        if (x == 0 || x == 1) && (y == 0 || y == 1) then
+          run (rest, (if x /= 0 && y /= 0 then (1, True) else (0, True)) : stack, state)
+        else
+          error "Run-time error"
     Neg -> case stack of
-      x : stack' -> run (rest, (if x /= 0 then 0 else 1) : stack', state)
+      (x, _) : stack -> run (rest, (if x /= 0 then (0, True) else (1, True)) : stack, state)
       _ -> error "Run-time error"
-    Fetch var -> let val = fromMaybe 0 (lookup var state) in run (rest, val : stack, state)
+    Fetch var -> let val = fromMaybe (0, False) (lookup var state) in run (rest, val : stack, state)
     Store var -> case stack of
-      x : stack' -> run (rest, stack', updateState var x state)
+      (x, isBool) : stack -> run (rest, stack, updateState var (x, isBool) state)
       _ -> error "Run-time error"
     Noop -> run (rest, stack, state)
     Branch c1 c2 -> case stack of
-      x : stack' -> if x /= 0 then run (c1 ++ rest, stack', state) else run (c2 ++ rest, stack', state)
+      (x, _) : stack -> if x /= 0 then run (c1 ++ rest, stack, state) else run (c2 ++ rest, stack, state)
       _ -> error "Run-time error"
     Loop c1 c2 -> case run (c1 ++ [Branch (c2 ++ [Loop c1 c2]) [Noop]], stack, state) of
-      (_, 0 : _, _) -> run (rest, stack, state)
-      (code', stack', state') -> run (code' ++ rest, stack', state')
+      (_, (0, _) : _, _) -> run (rest, stack, state)
+      (code', stack, state') -> run (code' ++ rest, stack, state')
+
+updateState :: String -> (Integer, Bool) -> State -> State
+updateState var val state = (var, val) : deleteBy (\(var1, _) (var2, _) -> var1 == var2) (var, val) state
 
 
-updateState :: String -> Integer -> State -> State
-updateState var x [] = [(var, x)]
-updateState var x ((v, _) : rest) | v == var = (var, x) : rest
-updateState var x (pair : rest) = pair : updateState var x rest
 
 -- To help you test your assembler
 testAssembler :: Code -> (String, String)
@@ -116,7 +113,11 @@ tests =
     ([Push (-20), Tru, Tru, Neg, Equ], ("False,-20", "")),
     ([Push (-20), Push (-21), Le], ("True", "")),
     ([Push 5, Store "x", Push 1, Fetch "x", Sub, Store "x"], ("", "x=4")),
-    ([Push 10, Store "i", Push 1, Store "fact", Loop [Push 1, Fetch "i", Equ, Neg] [Fetch "i", Fetch "fact", Mult, Store "fact", Push 1, Fetch "i", Sub, Store "i"]], ("", "fact=3628800,i=1"))
+    ([Push 10, Store "i", Push 1, Store "fact", Loop [Push 1, Fetch "i", Equ, Neg] [Fetch "i", Fetch "fact", Mult, Store "fact", Push 1, Fetch "i", Sub, Store "i"]], ("", "fact=3628800,i=1")),
+    ([Push 10, Store "n", Push 0, Store "a", Push 1, Store "b", Loop [Push 1, Fetch "n", Equ, Neg] [Fetch "a", Fetch "b", Add, Store "temp", Fetch "b", Store "a", Fetch "temp", Store "b", Push 1, Fetch "n", Sub, Store "n"]], ("", "a=34,b=55,n=1,temp=55")),
+    ([ Push 10, Store "n", Push 1, Store "result", Loop [ Push 1, Fetch "n", Equ, Neg ] 
+      [ Fetch "n", Fetch "result", Mult, Store "result", Push 1, Fetch "n", Sub, Store "n" ]], ("", "n=1,result=3628800"))
+      
   ]
 
 -- Function to run all tests
